7f1e3e25dd5480858050e4f4308dacb97175ab49
AllessRovee <111241460+AllessRovee@users.noreply.github.com>
2022-08-15 20:07:58 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+# 这是开源版本
+
+# 易支付API地址, 末尾需要包含/
+API = 'https://www.okx.com/'
+KEY = 'apikey = "cf9c4b5f-684b-47eb-ada7-ec5e223d1421"
+secretkey = "22D1E793996D747C1ABD56DABF79A85E"
+IP = ""
+
+# 支付成功跳转地址
+JUMP_URL = "https://kangle.bakbak.cn/paysuccess.html"
+# 支付超时时间(秒)
+PAY_TIMEOUT = 300
+# BOT API
+TOKEN = '5361729006:AAFETo8N8EtoRStHCdYRGSifLEfZnISUT4M'
+# ADMIN ID
+ADMIN_ID = [5497570732]
+
+# 管理员命令
+ADMIN_COMMAND_START = 'iadmin'
+ADMIN_COMMAND_QUIT = 'icancel'
